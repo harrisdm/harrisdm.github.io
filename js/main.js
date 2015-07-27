90b17d44b3050a3aac8e2ceed98ba97143422ba2
Mark Harris <harrisdm@hotmail.com>
2015-07-27 03:02:02 -0700

    side-bar animation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+  // Mobile menu toggling
   $('#toggle-menu').click(function(){
     $(this).toggleClass('menu-is-active');
     $(".mobile-menu").slideToggle();
@@ -36,5 +37,25 @@ $(document).ready(function() {
     window.scroll(0,0);
     $("a[href="+hash+"]").click();
   }
+
+
+
+
+
+  var $projects = $(".projects");
+  $projects.waypoint(function(direction) {
+    var t1 = new TimelineLite();
+    if(direction == "down") {
+      t1.to($(".side-profile-img"), 0.5, { top: "100px" })
+        .to($(".side-name"), 0.5, { left: "50%" }, "-=0.3")
+        .to($(".side-tag"), 0.5, { left: "50%" }, "-=0.3")
+        .to($(".side-button"), 0.5, { opacity: 1 }, "-=0.3");
+    } else {
+      t1.to($(".side-profile-img"), 0.5, { top: "-300px" })
+        .to($(".side-button"), 0.5, { opacity: 0 }, "-=0.5")
+        .to($(".side-tag"), 0.5, { left: "-500px" }, "-=1.0")
+        .to($(".side-name"), 0.5, { left: "-500px" }, "-=1.5");
+    }
+  }, { offset: "150px" });
   
 });
